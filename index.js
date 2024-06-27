e9cc3f4f1a950a5a0b760efcd43012c893bd6b25
Jonathan <japuentess@udistrital.edu.co>
2024-06-26 20:44:08 -0500

    Se añade server postgre y diferentes endpoints tentativos

---
+++ index.js
@@ -0,0 +1,43 @@
+// server/index.js
+import pg from 'pg';
+import {config} from 'dotenv';
+import express from 'express';
+
+config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,  
+})
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.get("/ping", async(req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  return res.json(result.rows[0]);
+});
+
+app.post("/login", (req, res) => {
+  if(err){
+    return res.json({ message: "Error al realizar el loggueo. Intente de nuevo." });
+  }
+});
+
+app.post("/registro", (req, res) => {
+  if(err){
+    return res.json({ message: "Error al realizar el registro. Intente de nuevo." });
+  }
+  res.json({ message: "Registro exitoso." });
+});
+
+app.get("/perfil", (req, res) => {
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
