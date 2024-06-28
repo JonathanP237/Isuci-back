130c96fe9ab63b44e9e22e7cc3ae11ca25786f4d
Jonathan <japuentess@udistrital.edu.co>
2024-06-28 15:09:05 -0500

    Se añade de validación de usuario y captcha para el login

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
 // server/index.js
 import pg from 'pg';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import express from 'express';
 
 config();
 
 const PORT = process.env.PORT || 3001;
-
+const bodyParser = require('body-parser');
 const app = express();
+const bcrypt = require('bcrypt');
+const idUsuarioActual = null;
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,  
+  connectionString: process.env.DATABASE_URL,
 })
 
+app.use(bodyParser.json());
+
+async function authenticateUser(idIngresado, contrasenaIngresada) {
+  const result = await pool.query("SELECT * FROM usuario WHERE idusuario = $1 LIMIT 1", [idIngresado]);
+  if (result.rows.length > 0) {
+    const user = result.rows[0];
+    idUsuarioActual = user.idusuario;    
+    // Compara la contraseña proporcionada con la contraseña hasheada almacenada
+    const passwordMatch = await bcrypt.compare(contrasenaIngresada, user.contrasenausuario);
+    return passwordMatch;
+  }
+  return false;
+}
+
+
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!" });
 });
 
-app.get("/ping", async(req, res) => {
+app.get("/ping", async (req, res) => {
   const result = await pool.query('SELECT NOW()');
   return res.json(result.rows[0]);
 });
 
-app.get("/test", async(req, res) => {
+app.get("/test", async (req, res) => {
   const result = await pool.query("SELECT * FROM usuario WHERE idtipousuario = 4 AND generousuario = 'M'");
-  return res.json(result.rows);
+  return res.json(result.rows[0].contrasenausuario);
 });
 
-app.post("/login", (req, res) => {
-  if(err){
-    return res.json({ message: "Error al realizar el loggueo. Intente de nuevo." });
+app.post("/login", async (req, res) => {
+  //const { usuario, password, recaptchaToken } = req.body;
+  const { usuario, password} = req.body;
+  // Primero, verifica el captcha
+  //const captchaValid = await verifyRecaptcha(recaptchaToken);
+  /*if (!captchaValid) {
+    return res.status(400).json({ message: "Fallo en la validación del captcha." });
+  }*/
+
+  // Luego, verifica las credenciales del usuario
+  const userAuthenticated = await authenticateUser(usuario, password);
+  if (!userAuthenticated) {
+    return res.status(401).json({ message: "Usuario o contraseña incorrectos." });
   }
+  // Si todo es correcto, procede con el login
+  res.json({ message: "Login exitoso." });
 });
 
 app.post("/registro", (req, res) => {
-  if(err){
+  if (err) {
     return res.json({ message: "Error al realizar el registro. Intente de nuevo." });
   }
   res.json({ message: "Registro exitoso." });
