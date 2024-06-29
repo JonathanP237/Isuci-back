8d48937a918fe6ae0e021f8394b71a947cbb9030
Jonathan <japuentess@udistrital.edu.co>
2024-06-29 13:22:45 -0500

    Correción error registro final

--- index.js
+++ index.js
@@ -90,13 +90,29 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/registro", async (req, res) => {
-  const sql = "INSERT INTO usuario (IDDOCUMENTO, IDTIPOUSUARIO, IDTIPOCONTEXTURA, IDPAIS, IDESPECIALIDAD, IDESCUADRA, TIPODOCUMENTOUSUARIO, NOMBREUSUARIO, APELLIDOUSUARIO, GENEROUSUARIO, CORREOUSUARIO, CONTRASENAUSUARIO, PESOUSUARIO, POTENCIAUSUARIO, ACELARACIONUSUARIO, VELOCIDADPROMEDIOUSUARIO, VELOCIDADMAXIMAUSUARIO, TIEMPOCICLISTA, ANOSEXPERIENCIA, GRADORAMPA) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  const sql = ` 
+    INSERT INTO usuario (
+      IDUSUARIO, IDDOCUMENTO, IDTIPOUSUARIO, IDTIPOCONTEXTURA, IDPAIS, IDESPECIALIDAD, IDESCUADRA, 
+      TIPODOCUMENTOUSUARIO, NOMBREUSUARIO, APELLIDOUSUARIO, GENEROUSUARIO, CORREOUSUARIO, 
+      CONTRASENAUSUARIO, PESOUSUARIO, POTENCIAUSUARIO, ACELARACIONUSUARIO, 
+      VELOCIDADPROMEDIOUSUARIO, VELOCIDADMAXIMAUSUARIO, TIEMPOCICLISTA, ANOSEXPERIENCIA, GRADORAMPA
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)
+  `;
+
+  const valores = [
+    req.body.idusuario, req.body.iddocumento, req.body.idtipousuario, req.body.idtipocontextura, req.body.idpais, 
+    req.body.idespecialidad, req.body.idescuadra, req.body.tipodocumentousuario, req.body.nombreusuario, 
+    req.body.apellidousuario, req.body.generousuario, req.body.correousuario, req.body.contrasenausuario, 
+    req.body.pesousuario, req.body.potenciausuario, req.body.acelaracionusuario, req.body.velocidadpromediousuario, 
+    req.body.velocidadmaximausuario, req.body.tiempociclista, req.body.anosexperiencia, req.body.gradorampa
+  ];
+
   try {
-    await pool.query(sql, [req.body.iddocumento, req.body.idtipousuario, req.body.idtipocontextura, req.body.idpais, req.body.idespecialidad, req.body.idescuadra, req.body.tipodocumentousuario, req.body.nombreusuario, req.body.apellidousuario, req.body.generousuario, req.body.correousuario, req.body.contrasenausuario, req.body.pesousuario, req.body.potenciausuario, req.body.acelaracionusuario, req.body.velocidadpromediousuario, req.body.velocidadmaximausuario, req.body.tiempociclista, req.body.anosexperiencia, req.body.gradorampa]);
-    return res.json({ message: "Registro exitoso." });
+    await pool.query(sql, valores);
+    res.json({ message: "Registro exitoso." });
   } catch (err) {
     console.error(err);
-    return res.json({ message: "Error al realizar el registro. Intente de nuevo." });
+    res.status(500).json({ message: "Error al realizar el registro. Intente de nuevo." });
   }
 });
 //VAlida tipo de usuario loggueado y construye los json con los datos del perfil requeridos
