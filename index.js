5f549139aae93f1c970c677fd9578a988eaccd99
Jonathan <japuentess@udistrital.edu.co>
2024-06-29 11:56:29 -0500

    corrección error perfil

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.post("/login", async (req, res) => {
     }
 
     //Valida el tipo de usuario que loguea  
-    return res.json({message: validarTipo()});
+    return res.json({message: await validarTipo()});
   } catch (error) {
     console.error(error);
     // Aquí puedes agregar manejo de errores más específico basado en el error devuelto
